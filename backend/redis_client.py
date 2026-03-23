b190fc35a6c2b43dd4f100fb885701e82755a33b
POWERTRICEPS <imnotdacoco05@gmail.com>
2026-03-23 00:11:39 -0700

    Added safeguard incase redis url dne

--- backend/redis_client.py
+++ backend/redis_client.py
@@ -7,4 +7,8 @@ load_dotenv()
 
 REDIS_URL = os.getenv("REDIS_URL")
 
+if not REDIS_URL:
+    raise ValueError("REDIS_URL environment variable is not set.")
+
 redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
+
